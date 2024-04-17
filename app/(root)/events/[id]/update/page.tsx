b93b66ba6e67acd60ff884ef535dc1ca50e426d6
rajabujshabani <rajabujshabani@gmail.com>
2024-04-17 20:01:49 +0300

    Implement react hook form from shadcn

--- app/(root)/events/[id]/update/page.tsx
+++ app/(root)/events/[id]/update/page.tsx
@@ -1,7 +1,7 @@
 import EventForm from "@/components/shared/EventForm"
 import { auth } from "@clerk/nextjs"
 
-const CreateEvent = () => {
+const UpdateEvent = () => {
   const { sessionClaims } = auth()
 
   const userId = sessionClaims?.userId as string
@@ -9,14 +9,14 @@ const CreateEvent = () => {
   return (
     <>
       <section className="bg-primary-50 bg-dotted-pattern bg-cover bg-center py-5 md:py-10">
-          <h3 className="wrapper text-center h3-bold sm:text-left">Create Event</h3>
+          <h3 className="wrapper text-center h3-bold sm:text-left">Update Event</h3>
      </section>
      <div className="wrapper my-8">
-          <EventForm  userId={userId} type='Create'/>
+          <EventForm  userId={userId} type='Update'/>
      </div>
     </>
    
 )
 }
 
-export default CreateEvent
+export default UpdateEvent
---
+++ components/shared/EventForm.tsx
@@ -0,0 +1,41 @@
+"use client"
+
+import { zodResolver } from "@hookform/resolvers/zod"
+import { useForm } from "react-hook-form"
+import { z } from "zod"
+
+ 
+const formSchema = z.object({
+  username: z.string().min(2, {
+    message: "Username must be at least 2 characters.",
+  }),
+})
+
+type EventFormProps = {
+    userId: string
+    type: 'Create' | 'Update'
+}
+
+const EventForm = ({ userId, type}: EventFormProps) => {
+
+    // 1. Define your form.
+    const form = useForm<z.infer<typeof formSchema>>({
+      resolver: zodResolver(formSchema),
+      defaultValues: {
+        username: "",
+      },
+    })
+    
+  // 2. Define a submit handler.
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    // Do something with the form values.
+    // ✅ This will be type-safe and validated.
+    console.log(values)
+  }
+
+  return (
+    <div>EventForm {type}</div>
+  )
+}
+
+export default EventForm
