933e30550bbd03a77c99ce98477501321672a9c3
rajabujshabani <rajabujshabani@gmail.com>
2024-04-17 21:01:09 +0300

    Implement zod validation

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -2,14 +2,20 @@
 
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-import { z } from "zod"
-
- 
-const formSchema = z.object({
-  username: z.string().min(2, {
-    message: "Username must be at least 2 characters.",
-  }),
-})
+import { Button } from "@/components/ui/button"
+import {
+  Form,
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form"
+import { Input } from "@/components/ui/input"
+import { eventFormSchema } from "@/lib/validator"
+import { z } from 'zod'
+import { eventDefaultValues } from "@/constants"
 
 type EventFormProps = {
     userId: string
@@ -17,25 +23,45 @@ type EventFormProps = {
 }
 
 const EventForm = ({ userId, type}: EventFormProps) => {
+  
+  const initialValues = eventDefaultValues;
 
     // 1. Define your form.
-    const form = useForm<z.infer<typeof formSchema>>({
-      resolver: zodResolver(formSchema),
-      defaultValues: {
-        username: "",
-      },
+    const form = useForm<z.infer<typeof eventFormSchema>>({
+      resolver: zodResolver(eventFormSchema),
+      defaultValues: initialValues
     })
-    
+
   // 2. Define a submit handler.
-  function onSubmit(values: z.infer<typeof formSchema>) {
+  function onSubmit(values: z.infer<typeof eventFormSchema>) {
     // Do something with the form values.
     // ✅ This will be type-safe and validated.
     console.log(values)
   }
 
   return (
-    <div>EventForm {type}</div>
-  )
+    <Form {...form}>
+    <form onSubmit={form.handleSubmit(onSubmit)} className="flex flex-col gap-5">
+
+      <div className="flex flex-col gap-5 md:flex-ro">
+          <FormField
+            control={form.control}
+            name="title"
+            render={({ field }) => (
+              <FormItem className="w-full">
+                <FormControl>
+                  <Input placeholder="Event title" {...field} className="input-field"/>
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+      </div>
+      
+      <Button type="submit">Submit</Button>
+    </form>
+  </Form>
+)
 }
 
 export default EventForm
