252ff9321595cc2e58141f313b3a40bad834f1d0
rajabujshabani <rajabujshabani@gmail.com>
2024-04-19 17:31:43 +0300

    implement file uploader

--- components/shared/EventForm.tsx
+++ components/shared/EventForm.tsx
@@ -17,6 +17,8 @@ import { eventFormSchema } from "@/lib/validator"
 import { z } from 'zod'
 import { eventDefaultValues } from "@/constants"
 import Dropdown from "./Dropdown"
+import { Textarea } from "@/components/ui/textarea"
+
 
 type EventFormProps = {
     userId: string
@@ -70,6 +72,21 @@ const EventForm = ({ userId, type}: EventFormProps) => {
             )}
           />
       </div>
+      <div className="flex flex-col gap-5 md:flex-row">
+          <FormField
+                control={form.control}
+                name="description"
+                render={({ field }) => (
+                  <FormItem className="w-full">
+                    <FormControl className="h-72">
+                      <Textarea placeholder="Description" {...field} className="textarea rounded-2xl"/>
+                    </FormControl>
+                    <FormMessage />
+                  </FormItem>
+                )}
+              />
+      </div>
+
       
       <Button type="submit">Submit</Button>
     </form>
