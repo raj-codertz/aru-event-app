7024eab897f5b198603a7fc297a28011ef4dc69d
rajabu shabani <rajabujshabani@gmail.com>
2024-05-26 08:01:12 +0300

    done implementing collection component

--- lib/actions/event.actions.ts
+++ lib/actions/event.actions.ts
@@ -1,11 +1,12 @@
 "use server"
 
-import { CreateEventParams } from "@/types"
+import { CreateEventParams, DeleteEventParams, GetAllEventsParams } from "@/types"
 import { handleError } from "../utils"
 import { connectToDatabase } from "../database"
 import User from "../database/models/user.model"
 import Event from "../database/models/event.model"
 import Category from "../database/models/category.model"
+import { revalidatePath } from "next/cache"
 
 const populateEvent = async (query: any) => {
     return query
@@ -50,3 +51,38 @@ export const getEventById = async ( eventId: string ) => {
         handleError(error)
     }
 }
+
+export const getAllEvents = async ( { query, limit = 6, page, category }: GetAllEventsParams) => {
+    try {
+        await connectToDatabase()
+
+        const conditions = {}
+
+        const eventsQuery = Event.find(conditions)
+            .sort({ createAt: 'desc'})
+            .skip(0)
+            .limit(limit)
+
+        const events = await populateEvent(eventsQuery)
+        const eventsCount = await Event.countDocuments(conditions)
+         
+        return {
+            data: JSON.parse(JSON.stringify(events)),
+            totalPages: Math.ceil(eventsCount / limit )
+        }
+
+    } catch (error) {
+        handleError(error)
+    }
+}
+
+export async function deleteEvent({ eventId, path }: DeleteEventParams) {
+    try {
+      await connectToDatabase()
+  
+      const deletedEvent = await Event.findByIdAndDelete(eventId)
+      if (deletedEvent) revalidatePath(path)
+    } catch (error) {
+      handleError(error)
+    }
+  }
